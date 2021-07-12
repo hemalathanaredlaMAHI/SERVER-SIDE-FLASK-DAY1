630ec6b509c483c33efd7b51c2176fd8a225b40c
hemalathanaredlaMAHI <41735211+hemalathanaredlaMAHI@users.noreply.github.com>
2021-07-12 20:19:18 +0530

    version 1

---
+++ day1.py
@@ -0,0 +1,32 @@
+
+class BankAccont():
+    def __init__(self,Owner):
+        self.balance=0.0
+        self.Owner=Owner
+    def Withdrawal(self,n):
+        if(self.balance<n):
+            return "Insufficient funds"
+        elif(n<0):
+            return "Amount can't be negative"
+        else:  
+            self.balance-=n
+            return self.Owner+" Your account balance is "+str(self.balance)
+    def deposit(self,n):
+        if(n<0):
+            return "Amount can't be negative"
+        self.balance+=n
+        return self.Owner+" Your account balance is "+str(self.balance)
+  
+name=input()
+n = int(input())
+w = BankAccont(name)
+for i in range(n):
+    s = input().split(" ")
+    if(s[0] == 'Withdrawal'):
+        print(w.Withdrawal(float(s[1])))
+    elif(s[0] == 'deposit'):
+        print(w.deposit(float(s[1])))
+    
+
+
+
